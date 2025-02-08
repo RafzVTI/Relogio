f0a92cfb93dfef9a8cac12267bd24dd7cd0b51f7
Rafael Paez <civic.vtieg6@outlook.com>
2025-02-08 19:13:38 -0300

    dia/mes/ano

--- javascript/re.js
+++ javascript/re.js
@@ -16,6 +16,18 @@ function atualizar(){
     diaSemana.forEach(dia => dia.classList.remove('diaatual'))
 
     diaSemana[diaAtual].classList.add('diaatual')
+
+    //data
+    let dia = agora.getDate().toString().padStart(2, '0')
+    let mes = (agora.getMonth() + 1).toString().padStart(2, '0')
+    let ano = agora.getFullYear()
+
+    document.getElementById('dia').textContent = dia
+    document.getElementById('mes').textContent = mes
+    document.getElementById('ano').textContent = ano
+
+    let dataformada = `${dia}/${mes}/${ano}`
+    document.getElementById('data').textContent = dataformada
 }
 
 setInterval(atualizar, 1000)
